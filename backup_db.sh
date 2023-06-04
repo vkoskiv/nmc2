a92c85d035f7d8298c23c8bf488f079c6c934cc9
Valtteri Koskivuori <vkoskiv@gmail.com>
2023-06-04 22:21:11 +0300

    Remove outdated FIXME
    pidfiles are now used, so the kill command just targets a single nmc2
    instance.

--- backup_db.sh
+++ backup_db.sh
@@ -6,7 +6,6 @@ cwd=`dirname $0`
 find $cwd/backups -mmin +10080 -type f -delete
 
 # Invoke the server to save a new backup
-# FIXME: Would be ideal to not send this signal to every running nmc2 instance
 kill -SIGUSR2 $(<"$cwd/nmc2.pid") || exit 1
 sleep 2
 
